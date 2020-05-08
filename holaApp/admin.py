f2214bb43bd7e97525386ba70ccac367622936cd
dcmacster <dcmacster@gmail.com>
2020-05-08 10:14:51 -0500

    primer comit

---
+++ holaApp/admin.py
@@ -0,0 +1,51 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Author, Genre, Book, BookInstance,Language
+
+#admin.site.register(Book)
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra=0
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+   list_display= ('title','author','display_genre')
+   inlines = [BooksInstanceInline]
+  
+
+# admin.site.register(Author)
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra=0
+    
+
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+   list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+   fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+   inlines= [BooksInline]
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+#admin.site.register(BookInstance)
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id','book','status','due_back')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        ('Datos del libro', {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+
+admin.site.register(Language)
+admin.site.register(Genre)
---
+++ holaApp/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+# Create your views here.
+@login_required
+def index(request):
+    """
+    Función vista para la página inicio del sitio.
+    """
+    # Genera contadores de algunos de los objetos principales
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+    # Libros disponibles (status = 'a')
+    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    num_authors=Author.objects.count()  # El 'all()' esta implícito por defecto.
+    # generos disponibles
+    num_genre=Genre.objects.all().count()
+    num_books_filter=Book.objects.filter(title__contains='one').count()
+    # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+    # Renderiza la plantilla HTML index.html con los datos en la variable contexto
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances, 'num_instances_available': num_instances_available,
+                 'num_authors': num_authors, 'num_genre': num_genre, 'num_books_filter': num_books_filter, 'num_visits': num_visits},
+    )
+
+class BookListView(LoginRequiredMixin, generic.ListView):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'
+    model = Book
+    paginate_by = 10
+
+
+class BookDetailView(LoginRequiredMixin, generic.DetailView):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'
+    model = Book
+
+class AuthorListView(LoginRequiredMixin, generic.ListView):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'
+    model = Author
+    paginate_by = 10
+
+
+class AuthorDetailView(LoginRequiredMixin, generic.DetailView):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'redirect_to'
+    model = Author
